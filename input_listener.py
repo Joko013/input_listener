059d064f50ab522393bf558a35c35055ddc414bc
Joko013 <30841710+Joko013@users.noreply.github.com>
2018-04-15 20:41:05 +0200

    V 0.2
    input to str

--- input_listener.py
+++ input_listener.py
@@ -2,18 +2,30 @@ import pythoncom
 import pyHook
 
 
-class Listener():
+class Listener(object):
+    """" Listener object to capture keyboard input. """
+
     def __init__(self):
+        self._return_pressed = False
+
+        # listen to the keyboard
         self.hm = pyHook.HookManager()
         self.hm.KeyDown = self._on_keyboard_event
         self.hm.HookKeyboard()
         pythoncom.PumpMessages()
 
     def _on_keyboard_event(self, event):
-        if event.Key == 'Return':
-            print('Nice, Return pressed')
-        else:
-            print('Return not pressed')
+
+        if event.Key == 'Return' and not self._return_pressed:
+            self._return_pressed = not self._return_pressed
+            self._text = ''
+
+        elif event.Key == 'Return' and self._return_pressed:
+            print(self._text)
+            self._return_pressed = not self._return_pressed
+
+        elif self._return_pressed:
+            self._text += event.Key
 
         # return True to pass the event to other handlers
         return True
