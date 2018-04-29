753367d983c839c3224b26ca99f42c616fc1ab9e
Joko013 <30841710+Joko013@users.noreply.github.com>
2018-04-29 17:40:34 +0200

    V 0.4
    only pressed keys

--- input_listener_keyboard.py
+++ input_listener_keyboard.py
@@ -5,40 +5,47 @@ import sys
 
 class Listener(object):
     """" Listener object to capture keyboard input. """
+
     def __init__(self):
-        self.keys_pressed = []
         self._start_listening()
-        
-        # here it should go like "on event X -> do Y" 
-        # (ie. when Enter is pressed, start recording, stop when pressed again)        
-        self._print_strings()
-        
-    def _wait_until_key(self, key):
-        """ Wait (do not record keyboard) until a key is pressed."""
-        keyboard.wait(key)
+
+        # here it should go like "on event X -> do Y"
+        # (ie. when Enter is pressed, start recording, stop when pressed again)
+        self._print_str()
 
     def _start_listening(self):
         """ Start listening when 'enter' is pressed. """
-        self._wait_until_key(key='enter')
-        keyboard.hook(self._log_event)
+        self._enter_pressed = False
+        self._esc_trigger = True
+
+        keyboard.on_press(self._log_event)
+        keyboard.wait('enter')
 
     def _log_event(self, event):
-        """ Log keyboard presses as keyboard events as json/dict to a list. """
+        """ Log pressed keyboard button names as strings. """
         e = event.to_json(ensure_ascii=sys.stdout.encoding != 'utf-8')
-        e_json = json.loads(e)
+        self.e_json = json.loads(e)
 
-        if e_json['event_type'] == 'down':
-            self.keys_pressed.append(e_json)
+        if self.e_json['name'] == 'enter' and not self._enter_pressed:
+            self.keys_pressed = ''
+            self._enter_pressed = not self._enter_pressed
 
-    def _print_strings(self):
-        """ Print typed strings. """
-        n=0
-        while n < 3:
-            self.keys_pressed = []
-            self._wait_until_key(key='enter')            
-            typed_string = ''.join(key['name'] for key in self.keys_pressed[:-1])
-            print(typed_string)
-            n+= 1
-            
+        elif self.e_json['name'] == 'enter' and self._enter_pressed:
+            self._enter_pressed = not self._enter_pressed
 
+        elif self.e_json['name'] == 'esc':
+            self._esc_trigger = not self._esc_trigger
+            # keyboard.send('enter') # how to exit here?
+
+        elif self._enter_pressed:
+            self.keys_pressed += self.e_json['name']
+
+    def _print_str(self):
+        n = 0
+        while self._esc_trigger:
+            keyboard.wait('enter')
+            self._enter_pressed = not self._enter_pressed
+            print(self.keys_pressed)
+            self.keys_pressed = ''
+            n += 1
 
