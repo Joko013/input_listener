7cc312dfb9b9741b0461bea83303d4d2236e6678
Joko013 <30841710+Joko013@users.noreply.github.com>
2018-05-29 13:07:48 +0200

    V 0.5
    Popup window and threading to keep loging input while printing message

--- input_listener_read_key.py
+++ input_listener_read_key.py
@@ -1,6 +1,8 @@
-import keyboard
 import json
 import sys
+import threading
+import keyboard
+from popups import make_popup
 
 
 class Listener(object):
@@ -12,29 +14,31 @@ class Listener(object):
 
         self._start_listening()
 
-
     def _start_listening(self):
         """ Listen to keypresses until 'esc' is pressed. """
 
         while self._esc_trigger:
             self._key_pressed = keyboard.read_event()
 
+            # t = threading.Thread(target=self._log_event, args=(self._key_pressed,))
+            # t.start()
             self._log_event(self._key_pressed)
-            
 
     def _log_event(self, event):
         """ Log pressed keyboard events (event_type == down). """
 
         e = event.to_json(ensure_ascii=sys.stdout.encoding != 'utf-8')
         self.e_json = json.loads(e)
-        
-        if self.e_json['event_type'] == 'down':
 
+        if self.e_json['event_type'] == 'down':
             if self.e_json['name'] == 'space' or self.e_json['name'] == 'enter':
-                print('You typed: {}'.format(self.keys_pressed))
+
+                to_print = self.keys_pressed
+                t = threading.Thread(target=make_popup, args=(to_print,))
+                t.start()
+                # make_popup('You typed: {}'.format(to_print))
             
             self._store_key()
-
 
     def _store_key(self):
         """ Log pressed keyboard names as strings.
